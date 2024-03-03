efa02c94c2e9da8de7c2c287589326df6030a1b3
tristanjayvirtudazo <82361353+tristanjayvirtudazo@users.noreply.github.com>
2024-03-04 05:20:57 +0800

    feat: :sparkles: adding database

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,18 +1,17 @@
-<?php 	
+<?php
 
-$localhost = "127.0.0.1";
-$username = "root";
-$password = "";
+$localhost = "128.199.248.115";
+$username = "developer";
+$password = "adminP@ssw0rd";
 $dbname = "stock";
-$port = "3307";
+$port = "3306";
 
 // db connection
 $connect = new mysqli($localhost, $username, $password, $dbname, $port);
 // check connection
-if($connect->connect_error) {
+if ($connect->connect_error) {
   die("Connection Failed : " . $connect->connect_error);
 } else {
   // echo "Successfully connected";
 }
 echo mysqli_error($connect);
-?>
