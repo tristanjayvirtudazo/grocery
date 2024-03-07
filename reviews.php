8e322f7033363f15103ffeb08b6831c799df5f92
tristanjayvirtudazo <82361353+tristanjayvirtudazo@users.noreply.github.com>
2024-03-07 22:21:18 +0800

    feat: reviews

--- reviews.php
+++ reviews.php
@@ -53,11 +53,12 @@ $result = $connect->query($fetchSql);
 <body>
   <br>
   <br><br>
-  <div class="container">
-    <div class="row" id="expiryTable">
-      <div class="panel panel-default">
-        <a href="./index.php">
-          < Back to Portal</a>
+  <a href="./index.php">
+    < Back to Portal</a>
+      <div class="container">
+        <div class="row" id="expiryTable">
+          <div class="panel panel-default">
+
             <div class="panel-heading">
               <div class="page-heading"> <i class="glyphicon glyphicon-edit"></i> Reviews</div>
 
@@ -113,93 +114,93 @@ $result = $connect->query($fetchSql);
               <!-- /table -->
 
             </div> <!-- /panel-body -->
-      </div> <!-- /panel -->
-    </div>
+          </div> <!-- /panel -->
+        </div>
 
-    <!-- add review -->
-    <div class="modal fade" tabindex="-1" role="dialog" id="addReviewModal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title"><i class="glyphicon glyphicon-eye"></i> Add Review</h4>
-          </div>
-          <div class="modal-body">
+        <!-- add review -->
+        <div class="modal fade" tabindex="-1" role="dialog" id="addReviewModal">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"><i class="glyphicon glyphicon-eye"></i> Add Review</h4>
+              </div>
+              <div class="modal-body">
 
-            <form class="form-horizontal" id="postReviewForm" action="" method="POST">
-              <br />
+                <form class="form-horizontal" id="postReviewForm" action="" method="POST">
+                  <br />
 
-              <input type="text" class="hidden" name="branchId" id="branchId" value="<?php echo $_GET['branchId']; ?>">
+                  <input type="text" class="hidden" name="branchId" id="branchId" value="<?php echo $_GET['branchId']; ?>">
 
-              <div class="form-group">
-                <label for="fullName" class="col-sm-3 control-label">Full Name: </label>
-                <div class="col-sm-8">
-                  <input type="text" class="form-control" id="fullName" placeholder="Full Name" name="fullName" autocomplete="off">
-                </div>
-              </div> <!-- /form-group-->
+                  <div class="form-group">
+                    <label for="fullName" class="col-sm-3 control-label">Full Name: </label>
+                    <div class="col-sm-8">
+                      <input type="text" class="form-control" id="fullName" placeholder="Full Name" name="fullName" autocomplete="off">
+                    </div>
+                  </div> <!-- /form-group-->
 
-              <div class="form-group">
-                <label for="email" class="col-sm-3 control-label">Email: </label>
-                <div class="col-sm-8">
-                  <input type="email" class="form-control" id="email" placeholder="Email" name="email" autocomplete="off">
-                </div>
-              </div> <!-- /form-group-->
+                  <div class="form-group">
+                    <label for="email" class="col-sm-3 control-label">Email: </label>
+                    <div class="col-sm-8">
+                      <input type="email" class="form-control" id="email" placeholder="Email" name="email" autocomplete="off">
+                    </div>
+                  </div> <!-- /form-group-->
 
-              <div class="form-group">
-                <label for="comment" class="col-sm-3 control-label">Comment: </label>
-                <div class="col-sm-8">
-                  <textarea class="form-control" name="comment" id="comment" cols="30" rows="10"></textarea>
-                </div>
-              </div> <!-- /form-group-->
+                  <div class="form-group">
+                    <label for="comment" class="col-sm-3 control-label">Comment: </label>
+                    <div class="col-sm-8">
+                      <textarea class="form-control" name="comment" id="comment" cols="30" rows="10"></textarea>
+                    </div>
+                  </div> <!-- /form-group-->
 
-              <div class="form-group">
-                <label for="captcha" class="col-sm-3 control-label">Captcha </label>
-                <div class="col-sm-8">
-                  <div class="h-captcha" data-sitekey="59e2ba2f-a50a-414b-9dd6-dec59d778b1d"></div>
-                </div>
+                  <div class="form-group">
+                    <label for="captcha" class="col-sm-3 control-label">Captcha </label>
+                    <div class="col-sm-8">
+                      <div class="h-captcha" data-sitekey="59e2ba2f-a50a-414b-9dd6-dec59d778b1d"></div>
+                    </div>
+                  </div>
+
+
+                  <div class="modal-footer editProductFooter1">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+
+                    <button type="submit" class="btn btn-success" id="postBtn" name="postBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-ok-sign"></i> Post</button>
+                  </div> <!-- /modal-footer -->
+                </form> <!-- /.form -->
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+        <!-- /categories brand -->
+
+        <!-- Edit dialouge -->
+        <div class="modal fade" id="editDialog" tabindex="-1" role="dialog">
+          <div class="modal-dialog">
+            <div class="modal-content">
+
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title dynamic-title"><i class="fa fa-edit"></i></h4>
               </div>
 
+              <div class="modal-body" style="max-height:450px; overflow:auto;">
+                <div class="text-center">
+                  <h5>Would you like to proceed?</h5>
+                </div>
 
-              <div class="modal-footer editProductFooter1">
+              </div> <!-- /modal-body -->
+
+              <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
-
-                <button type="submit" class="btn btn-success" id="postBtn" name="postBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-ok-sign"></i> Post</button>
-              </div> <!-- /modal-footer -->
-            </form> <!-- /.form -->
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-    <!-- /categories brand -->
-
-    <!-- Edit dialouge -->
-    <div class="modal fade" id="editDialog" tabindex="-1" role="dialog">
-      <div class="modal-dialog">
-        <div class="modal-content">
-
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title dynamic-title"><i class="fa fa-edit"></i></h4>
-          </div>
-
-          <div class="modal-body" style="max-height:450px; overflow:auto;">
-            <div class="text-center">
-              <h5>Would you like to proceed?</h5>
+                <button type="button" class="btn btn-primary" id="dialogBtn" data-toggle="modal" data-dismiss="modal"> <i class="glyphicon glyphicon-ok-sign"></i> Proceed</button>
+              </div>
             </div>
-
-          </div> <!-- /modal-body -->
-
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
-            <button type="button" class="btn btn-primary" id="dialogBtn" data-toggle="modal" data-dismiss="modal"> <i class="glyphicon glyphicon-ok-sign"></i> Proceed</button>
+            <!-- /modal-content -->
           </div>
+          <!-- /modal-dailog -->
         </div>
-        <!-- /modal-content -->
-      </div>
-      <!-- /modal-dailog -->
-    </div>
 
-  </div>
+      </div>
 </body>
 
 </html>
