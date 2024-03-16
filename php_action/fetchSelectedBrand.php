c0e168bba1effce967e8f7e6c385c84a0e609f9c
tristanjayvirtudazo <82361353+tristanjayvirtudazo@users.noreply.github.com>
2024-03-16 18:26:24 +0800

    fix: :bug: change the equality of brands category and the categories itself

--- php_action/fetchSelectedBrand.php
+++ php_action/fetchSelectedBrand.php
@@ -5,7 +5,7 @@ require_once 'core.php';
 $brandId = $_POST['brandId'];
 
 $sql = "SELECT brands.brand_id, brands.brand_name, brands.brand_active, brands.brand_status, categories.categories_id, categories.categories_name FROM brands
-        INNER JOIN categories ON categories.categories_id = brands.brand_id
+        INNER JOIN categories ON categories.categories_id = brands.category
         WHERE brands.brand_id = $brandId";
 $result = $connect->query($sql);
 
